fed72ca97b119988f67e9f827f4d9264c4bb728b
sudoBash418 <sudoBash418@gmail.com>
2022-09-25 02:53:24 -0600

    pyinfra/deploys: Lock root password on initial deploy

--- pyinfra/deploys/initial.py
+++ pyinfra/deploys/initial.py
@@ -1,7 +1,12 @@
 # Initial deploy - prepares the system for later deployments
 
+from pyinfra.operations import server
+
 from roles import base
 
 
 # Initial system preparation
 base.apply()
+
+# Ensure root login is disabled
+server.shell("passwd -l root")
