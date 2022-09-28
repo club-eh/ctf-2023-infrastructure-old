7107f4fac7a678076191dccaf79045165d69ceb2
sudoBash418 <sudoBash418@gmail.com>
2022-09-27 21:10:34 -0600

    pyinfra/util: Improve get_secrets_dir() error checking

--- pyinfra/util/secrets.py
+++ pyinfra/util/secrets.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from pyinfra import host
+from pyinfra.api import DeployError
 
 
 BASE_DIRECTORY = Path("../secrets")
@@ -14,7 +15,17 @@ def get_secrets_dir(env_name: str | None = None) -> Path:
 		Defaults to the current host's env_name.
 	"""
 
+	# default to current host's environment
 	if env_name is None:
 		env_name = host.data.env_name
 
-	return BASE_DIRECTORY / env_name
+		if env_name is None:
+			raise DeployError("Could not determine environment from current host (missing `env_name` data)")
+
+	secrets_dir =  BASE_DIRECTORY / env_name
+
+	# ensure the path exists
+	if not secrets_dir.exists():
+		raise DeployError(f"Secrets directory not found (should be at '{secrets_dir}')")
+
+	return secrets_dir
