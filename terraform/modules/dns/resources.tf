4994fe1023f66d4aeb2c16a4359fe9e211494000
sudoBash418 <sudoBash418@gmail.com>
2022-11-01 03:53:25 -0600

    terraform: Style fixes

--- terraform/modules/dns/resources.tf
+++ terraform/modules/dns/resources.tf
@@ -3,15 +3,15 @@ resource "cloudflare_record" "flagship" {
   zone_id = data.cloudflare_zone.base.id
   type    = "A"
   ttl     = 300 # 5 minutes
-  name  = "${var.subdomain}"
-  value = var.ip_flagship
+  name    = var.subdomain
+  value   = var.ip_flagship
 }
 resource "cloudflare_record" "flagship_wildcard" {
   zone_id = data.cloudflare_zone.base.id
   type    = "A"
   ttl     = 300 # 5 minutes
-  name  = "*.${var.subdomain}"
-  value = var.ip_flagship
+  name    = "*.${var.subdomain}"
+  value   = var.ip_flagship
 }
 
 # DNS records for challenge servers (cs#.ctf-2023.clubeh.ca)
